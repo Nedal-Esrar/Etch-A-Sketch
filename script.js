3ca3b3fa35f789886c9ccf52da135f72e72d4bd6
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-05-22 12:00:42 +0300

    fix footer

---
+++ script.js
@@ -0,0 +1,10 @@
+// Default Constants
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE =  'color';
+const DEFAULT_GRID_SIZE = 16;
+
+// states
+let color = DEFAULT_COLOR;
+let mode = DEFAULT_MODE;
+let gridSize = DEFAULT_GRID_SIZE;
+
